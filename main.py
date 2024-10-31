15d2da5168be61e1f360023c8561025d1f124fc1
vbifib <newxforcoin@gmail.com>
2024-10-31 16:43:52 +0300

    Homework lesson3 commit

--- main.py
+++ main.py
@@ -1,2 +1,28 @@
 # Hi Slava!
 # How are you doing?
+
+firstname = input("Как тебя зовут? ")
+lastname = input("Укажите фамилию: ")
+age = input("Введите свой возраст (лет): ")
+print(f"Привет, меня зовут {firstname} {lastname}, мне {age} лет.")
+
+some_str = input("Введите любую строку: ")
+print(some_str[2])
+
+some_str2 = input("Введите любую строку : ")
+some_str3 = some_str2[-2]
+print(some_str3)
+
+some_str4 = input("Введите любую строку : ")
+some_str5 = some_str4[:5]
+print(some_str5)
+
+some_str6 = input("Введите любую строку : ")
+some_str7 = some_str6[0:-2]
+print(some_str7)
+
+some_str8 = "Hello, World!"
+a = "Mike"
+print(some_str8.replace('World', a))
+
+
