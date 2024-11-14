36b56ed748850234eaf3f4b04c969a9cd342c296
vbifib <newxforcoin@gmail.com>
2024-11-14 10:06:30 +0300

    added lesson7 from ht-7

---
+++ lesson7.py
@@ -0,0 +1,19 @@
+class MyClass:
+    # cls_atr = 11
+    name: str
+
+    def __init__(self, name):
+        self.name = name
+
+    # def say_something(self):
+    #     return f"I say: {self.name}"
+
+student_1 = MyClass('Anna')
+student_2 = MyClass('Lena')
+
+print(student_1)
+print(student_2)
+
+
+
+
